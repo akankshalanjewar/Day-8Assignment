13032823587234e61c92cd09d72e6dacc5c89229
akankshalanjewar <akankshalanjewar02@gmail.com>
2022-03-16 09:51:30 +0530

    [Akanksha]: Calculating Length of line

--- UC-1/Program.cs
+++ UC-1/Program.cs
@@ -2,11 +2,32 @@
 
 namespace UC_1
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+      public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double x1, x2, y1, y2;
+
+            Console.WriteLine("Enter The coordinate of x1: ");
+            x1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter The coordinate of x2: ");
+            x2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter The coordinate of y1: ");
+            y1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter The coordinate of y2:");
+            y2 = int.Parse(Console.ReadLine());
+
+
+            double Length;
+
+            Length = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+            Console.WriteLine("Length of line : " + Length);
+            Console.ReadLine();
+
+
         }
     }
 }
